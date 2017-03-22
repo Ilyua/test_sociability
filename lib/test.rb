6908fe63bab8b65f53030ba93b9017499632d6ee
Ilyua <old.flanigan@gmail.com>
2017-03-22 14:58:37 +0300

    Correct style mistakes

--- lib/test.rb
+++ lib/test.rb
@@ -1,15 +1,17 @@
 class Test
+  attr_reader :get_yes_answers, :get_sometimes_answers
+
   def initialize
     @questions = File.readlines("#{File.dirname(__FILE__)}/../data/questions.txt")
     @yes_answers = 0
     @sometimes_answers = 0
   end
 
-  def start
-    if ARGV[0] == nil
+  def start(name)
+    if name == nil
       user_name = "Незнакомец"
     else
-      user_name = ARGV[0]
+      user_name = name
     end
 
     puts "Привет, #{user_name}"
@@ -23,27 +25,17 @@ class Test
       puts item
       user_answer = nil
 
-      while user_answer != 1 &&
-            user_answer != 2 &&
-            user_answer != 3 do
-          puts "Отвечайте yes(1),no(2) sometimes(3)"
-          user_answer = STDIN.gets.to_i
-        end
+      unless (1..3).include?(user_answer)
+        puts "Отвечайте yes(1),no(2) sometimes(3)"
+        user_answer = STDIN.gets.to_i
+      end
 
-        case user_answer
-        when 1
-          @yes_answers += 1
-        when 3
-          @sometimes_answers += 1
-        end
+      case user_answer
+      when 1
+        @yes_answers += 1
+      when 3
+        @sometimes_answers += 1
+      end
     end
-  end
-
-  def get_yes_answers
-    @yes_answers
-  end
-
-  def get_sometimes_answers
-    @sometimes_answers
   end
 end
--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative "lib/test"
 require_relative "lib/result"
 
 test = Test.new
-test.start
+test.start(ARGV[0])
 test.testing
 
 result = Result.new(test.get_yes_answers,test.get_sometimes_answers)
