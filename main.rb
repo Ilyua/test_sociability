79c7a4a1ae50cbc3cc7a748c6b0e25ca5d484d7d
Ilyua <old.flanigan@gmail.com>
2017-03-23 22:16:30 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -5,5 +5,5 @@ test = Test.new
 test.start(ARGV[0])
 test.testing
 
-result = Result.new(test.get_yes_answers,test.get_sometimes_answers)
+result = Result.new(test.get_yes_answers, test.get_sometimes_answers)
 result.print_result
